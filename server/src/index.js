e72d3e973aafa6a43dc41410a7829c64f885533d
Amrit Kaur <amritkaur01001@gmail.com>
2026-03-30 02:58:21 +0530

    fix firebase private key parsing

--- server/src/index.js
+++ server/src/index.js
@@ -31,7 +31,7 @@ app.use((err, req, res, next) => {
 });
 
 // Connect to MongoDB then start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(process.env.MONGO_URI)
